f4085268c435b7b40d2d7564fa8950053e10ecad
gustavgamstedt <gustav.gamstedt@outlook.com>
2025-09-02 14:50:35 +0200

    added formula labb1

--- Labb1_del2.py
+++ Labb1_del2.py
@@ -1,13 +1,15 @@
+from math import pi
 # Constants
 G = 6.674 * pow(10, -11)
 
 # Prompt the user
-avstånd = float(input("Ange avstånd (m): "))
-omloppstid = float(input("Ange omloppstid (s): "))
+avstånd = float(input("Ange avstånd (km): "))*1000
+omloppstid = float(input("Ange omloppstid (dygn): "))*24*60**2
 
 # Lambda dunction for mass
-massa = lambda d, T: d * T
-fix
+massa = (4*pow(pi,2)*pow(avstånd,3))/(G*pow(omloppstid,2))
+# fix
 if __name__ == '__main__':
     # Calculate and print the earth's mass
-    print("Jordens massa: " + str(massa(avstånd, omloppstid)) + " gk") f string
+    print(f"Jordens massa: {massa:.2e} k")
+    

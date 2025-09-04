b4abb141ca0c1a49655e8e63977da7bdad42098a
abbnoamar <116065732+Lundalex@users.noreply.github.com>
2025-09-04 08:43:08 +0200

    sm

--- Labb1_del2.py
+++ Labb1_del2.py
@@ -2,14 +2,13 @@ from math import pi
 # Constants
 G = 6.674 * pow(10, -11)
 
-# Prompt the user
-avstånd = float(input("Ange avstånd (km): "))*1000
-omloppstid = float(input("Ange omloppstid (dygn): "))*24*60**2
-
 # Lambda function for mass
-massa = (4*pow(pi,2)*pow(avstånd,3))/(G*pow(omloppstid,2))
+massa = lambda s, T : (4*pow(pi,2)*pow(s,3))/(G*pow(T,2))
 # fix
 if __name__ == '__main__':
+    # Prompt the user
+    avstånd = float(input("Ange avstånd (km): "))*1000
+    omloppstid = float(input("Ange omloppstid (dygn): "))*24*60**2
+
     # Calculate and print the earth's mass
-    print(f"Jordens massa: {massa:.2e} k")
-    
+    print(f"Jordens massa: {massa(avstånd, omloppstid):.2e} k")

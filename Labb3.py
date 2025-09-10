67c69298ef6c7d6f43404e510be36f4a008f8352
gustavgamstedt <gustav.gamstedt@outlook.com>
2025-09-10 10:25:25 +0200

    lite förklarande text L3

--- Labb3.py
+++ Labb3.py
@@ -1,15 +1,17 @@
 # hur många av orden som ska printas
-printa_st: int = 500
+printa_st: int = 10
 
 def lasInOrdenFranFil(fil:str, ord:bool = False) -> str:
-    def replace_many(text:str,ersätt_lista:list):
+    '''Läser in orden från filen vi väljer'''
+    def ersätt_flera(text:str,ersätt_lista:list):
+        '''En inre funktion som tar bort tecknen i ersätt_lista'''
         for sträng in ersätt_lista:
             text = text.replace(sträng, '')
         return text
     with open(fil, 'r') as f:
         text = f.read()
     if not ord: # tar bort lite skräp om det är från artikeln. Måste göras innan split.
-        text = replace_many(text, ['.', ',', '-', '–', ':','(',')'])
+        text = ersätt_flera(text, ['.', ',', '-', '–', ':','(',')'])
     # delar upp orden ord för ord.
     split_text = text.lower().replace('\n',' ').split(' ')
     return split_text
@@ -28,7 +30,7 @@ def raknaOvanliga(artikel:list[str], unika_ord:str) -> list:
         # Vi ersätter unika ordet med [unika ordet, antal av det i artikeln] 
         unika_ord[i] = (ord, artikel.count(ord))
     # vi sorterar listan efter andra värdet, alltså count värdet ovan. Sedan flippar vi runt listan i andra hållet
-    sorterat = sorted(unika_ord, key=lambda par: par[1], reverse=True)
+    sorterat = sorted(unika_ord, key=lambda par: par[1], reverse=True) # Här är key vad den sorterar på. I vårt fall andra värdet i listan alltså ordet och inte antalet
     return sorterat
 
 def printaLista(lista):
